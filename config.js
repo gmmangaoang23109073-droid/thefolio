4de8999c3a09f0495449a73d0703071433fed2f2
Glaiza Mangaoang <gmmangaoang23109073@student.dmmmsu.edu.ph>
2026-04-11 16:31:06 +0800

    fix: move config.js to frontend/src

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// Use Render backend in production, localhost in development
 export const BACKEND_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://thefolio-mpb8.onrender.com'   // <-- your Render backend URL
+  ? 'https://thefolio-mpb8.onrender.com'
   : 'http://localhost:5000';
